ed8eb770bc2b4502e0ce11313725437aba777a14
NandoCodeBR <52359881+NandoCodeBR@users.noreply.github.com>
2019-12-18 21:29:43 -0300

    Update Tarefa_controller.php

--- Controller/Tarefa_controller.php
+++ Controller/Tarefa_controller.php
@@ -2,9 +2,7 @@
 require 'TarefaModel.php';
 require 'Conexao.php';
 require 'TarefaService.php';
-echo '<pre>';
-print_r($_POST['tarefa']);
-echo '</pre>';
+
 
 $tarefa = new tarefa(); 
 $tarefa->__set('tarefa', $_POST['tarefa']);
@@ -13,7 +11,6 @@ $conexao = new Conexao();
 
 $service = new TarefaService($conexao, $tarefa);
 $service->Criar();
-echo '<pre>';
-print_r($service);
-echo '</pre>';
+
+header('Location: nova_tarefa.php?inclusao=1');
 ?>
