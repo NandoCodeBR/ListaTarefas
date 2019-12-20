de062ff58232ae75ca486a68021335c6ceb27beb
NandoCodeBR <52359881+NandoCodeBR@users.noreply.github.com>
2019-12-19 22:09:58 -0300

    Add files via upload

--- Controller/TarefaService.php
+++ Controller/TarefaService.php
@@ -18,7 +18,15 @@ Class TarefaService{
         $stmt->execute();
     }
     public function Recuperar(){ //Read
-
+        $query = 'select'
+                . ' t.id, s.status, tarefa'
+                . ' from'
+                . ' tb_tarefas as t'
+                . ' left join tb_status as s on (t.id_status = s.id)';
+        $stmt = $this->conexao->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+        
     }
     public function Atualizar (){ //Update
 
--- Controller/Tarefa_controller.php
+++ Controller/Tarefa_controller.php
@@ -3,7 +3,9 @@ require 'TarefaModel.php';
 require 'Conexao.php';
 require 'TarefaService.php';
 
+$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
+if ($acao =='inserir') {
 $tarefa = new tarefa(); 
 $tarefa->__set('tarefa', $_POST['tarefa']);
 
@@ -13,4 +15,11 @@ $service = new TarefaService($conexao, $tarefa);
 $service->Criar();
 
 header('Location: nova_tarefa.php?inclusao=1');
+} else if ($acao == 'recuperar') {
+    
+    $tarefa = new tarefa();
+    $conexao = new Conexao();
+    $tarefaService = new TarefaService($conexao, $tarefa);
+    $tarefas = $tarefaService->Recuperar();
+}
 ?>
